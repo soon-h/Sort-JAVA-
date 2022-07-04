3b8bd663b90e6fa525caf9e3b6db52cb2557ec3f
soon-h <ksh8895420@gmail.com>
2022-07-04 23:20:06 +0900

    first commit

---
+++ selection_sort.java
@@ -0,0 +1,19 @@
+public class selection_sort {
+    public static void sort(int[] arr){
+        for(int i = 0; i < arr.length - 1; i++){
+            int minIndex = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+                swap(arr, i , minIndex);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
